4bd00d80fa75edde86e0b348bee2b20db1344d71
Lukas Burk <burk@leibniz-bips.de>
2022-03-07 15:44:08 +0100

    Preliminary hardhat interface

--- R/predict_rpf.R
+++ R/predict_rpf.R
@@ -1,20 +1,74 @@
 #' Random Planted Forest Predictions
 #'
 #' @param object A fit object as returned by [`rpf()`].
-#' @param new_data Matrix or vector input for new observations.
-#' @param components TODO
+#' @param new_data Input for new observations.
+#' @param components [`0`]: TODO
 #' @param ... Unused.
 #'
-#' @return Vector of predictions (FIXME: Type, dimension, response vs. prob)
+#' @return For regression: A data.frame with column `.pred` with the same
+#' number of rows as `new_data`.
+#' For Classification: TBI
+#'
 #' @export
+#' @importFrom hardhat forge validate_prediction_size
 #'
 #' @examples
 #' \dontrun{
-#' # This doesn't work just yet
-#'  rpfit <- rpf(mtcars$mpg, as.matrix(mtcars[, c("cyl", "wt")]))
-#'  predict(rpfit, mtcars[1:5, c("cyl", "wt")], components = 1)
+#' rpfit <- rpf(y = mtcars$mpg, x = as.matrix(mtcars[, c("cyl", "wt")]))
+#' predict(rpfit, mtcars[, c("cyl", "wt")], components = 0)
 #' }
-predict.Rcpp_RandomPlantedForest <- function(object, new_data, components, ...){
-  if(is.matrix(new_data)) return(object$predict_matrix(new_data, components))
-  if(is.vector(new_data)) return(object$predict_vector(new_data, components))
+predict.rpf <- function(object, new_data, type = "numeric",
+                        components = 0, ...) {
+
+  # Enforces column order, type, column names, etc
+  processed <- hardhat::forge(new_data, object$blueprint)
+
+  out <- predict_rpf_bridge(type, object, processed$predictors, components, ...)
+
+  hardhat::validate_prediction_size(out, new_data)
+
+  out
+}
+
+
+#' Bridge: Passes new data to corresponding predict function
+predict_rpf_bridge <- function(type, object, predictors, ...) {
+
+  type <- match.arg(type, choices = c("numeric", "class", "prob"))
+
+  predictors <- as.matrix(predictors)
+
+  switch(
+    type,
+    numeric = predict_rpf_numeric(object, predictors, ...),
+    class = predict_rpf_class(object, predictors, ...),
+    prob = predict_rpf_prob(object, predictors, ...)
+  )
+}
+
+#' Predict function for numeric outcome / regression
+predict_rpf_numeric <- function(object, new_data, components, ...){
+  pred <- object$fit$predict_matrix(new_data, components)
+  out <- hardhat::spruce_numeric(pred)
+
+  out
+}
+
+
+# Classification ----------------------------------------------------------
+
+#' Predict function for classification: Class prediction
+predict_rpf_class <- function(object, new_data, components, ...){
+  pred <- object$fit$predict_matrix(new_data, components)
+  out <- hardhat::spruce_class(pred)
+
+  out
+}
+
+#' Predict function for classification: Probability prediction
+predict_rpf_prob <- function(object, new_data, components, ...){
+  pred <- object$fit$predict_matrix(new_data, components)
+  out <- hardhat::spruce_prob(pred)
+
+  out
 }
--- R/rpf.R
+++ R/rpf.R
@@ -34,10 +34,10 @@
 #' # Regression with formula
 #' rpfit <- rpf(mpg ~ cyl + wt, data = mtcars)
 #' }
-rpf <- function(x, max_interaction = 1, ntrees = 50, splits = 30,
+rpf <- function(x, ..., max_interaction = 1, ntrees = 50, splits = 30,
                 split_try = 10, t_try = 0.4, deterministic = FALSE,
                 parallel = FALSE, purify = FALSE, cv = FALSE,
-                loss = "L2", delta = 0, epsilon = 0.1, ...) {
+                loss = "L2", delta = 0, epsilon = 0.1) {
   UseMethod("rpf")
 }
 
@@ -119,14 +119,21 @@ new_rpf <- function(fit, blueprint) {
 rpf_impl <- function(Y, X, max_interaction = 1, ntrees = 50, splits = 30, split_try = 10, t_try = 0.4,
                 deterministic = FALSE, parallel = FALSE, purify = FALSE, cv = FALSE,
                 loss = "L2", delta = 0, epsilon = 0.1) {
-  if (!missing(loss) | !missing(delta) | !missing(epsilon)) {
-    return(new(ClassificationRPF, Y, X, loss, c(
+
+  # FIXME: Proper classif/regression detection and switching
+
+  # Assume binary Y for classif
+  if (length(unique(Y)) == 2) {
+    fit <- new(ClassificationRPF, Y, X, loss, c(
       max_interaction, ntrees, splits, split_try, t_try,
       purify, deterministic, parallel, cv, delta, epsilon
-    )))
+    ))
+  } else {
+    fit <- new(RandomPlantedForest, Y, X, c(
+      max_interaction, ntrees, splits, split_try, t_try,
+      purify, deterministic, parallel, cv
+    ))
   }
-  return(new(RandomPlantedForest, Y, X, c(
-    max_interaction, ntrees, splits, split_try, t_try,
-    purify, deterministic, parallel, cv
-  )))
+
+  fit
 }
