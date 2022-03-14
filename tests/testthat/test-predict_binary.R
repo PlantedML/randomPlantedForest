50769b94adfe5aad5f0065e9daab60dbcea2b4c0
Marvin Wright <github@wrig.de>
2022-03-14 17:26:25 +0100

    fix tests for classif prediction

--- tests/testthat/test-predict_binary.R
+++ tests/testthat/test-predict_binary.R
@@ -11,19 +11,24 @@ xdat <- data.frame(
 test_that("Probability prediction: y = factor / L2 loss", {
   bin_fit <- rpf(yfact ~ x1 + x2, data = xdat, loss = "L2")
   
-  bin_pred <- predict(bin_fit_L2, new_data = xdat, type = "prob")
+  bin_pred <- predict(bin_fit, new_data = xdat, type = "prob")
+  expect_equal(dim(bin_pred), c(nrow(xdat), nlevels(xdat$yfact)))
+  expect_gte(min(bin_pred), 0)
+  expect_lte(max(bin_pred), 1)
 })
 
 test_that("Class prediction: y = factor / L2 loss", {
   bin_fit <- rpf(yfact ~ x1 + x2, data = xdat, loss = "L2")
   
   bin_pred <- predict(bin_fit, new_data = xdat, type = "class")
-  
+  expect_equal(dim(bin_pred), c(nrow(xdat), 1))
+  expect_true(all(bin_pred$.pred_class %in% levels(xdat$yfact)))
 })
 
 test_that("Class prediction: y = 0,1 / L2 loss", {
   bin_fit <- suppressWarnings(rpf(y01 ~ x1 + x2, data = xdat, loss = "L2"))
   
   bin_pred <- predict(bin_fit, new_data = xdat, type = "class")
-  
+  expect_equal(dim(bin_pred), c(nrow(xdat), 1))
+  expect_true(all(bin_pred$.pred_class %in% c("0", "1")))
 })
