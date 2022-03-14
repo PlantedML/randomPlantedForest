7664a807d8ebe833d47a2577dbb274bfa11bfcda
Lukas Burk <lukas@quantenbrot.de>
2022-03-14 16:14:41 +0100

    Test for setting seed: also test for expected different result

--- tests/testthat/test-rpf.R
+++ tests/testthat/test-rpf.R
@@ -27,10 +27,15 @@ test_that("Setting seed in R works", {
   set.seed(13)
   rpf_fit1 <- rpf(mpg ~ wt + cyl, data = mtcars)
   pred1 <- predict(rpf_fit1, mtcars[1:5, ])
-
+  
   set.seed(13)
   rpf_fit2 <- rpf(mpg ~ wt + cyl, data = mtcars)
   pred2 <- predict(rpf_fit2, mtcars[1:5, ])
-
+  
+  # No seed set, so should be different
+  rpf_fit3 <- rpf(mpg ~ wt + cyl, data = mtcars)
+  pred3 <- predict(rpf_fit3, mtcars[1:5, ])
+  
   expect_equal(pred1, pred2)
+  expect_failure(expect_equal(pred1, pred3))
 })
