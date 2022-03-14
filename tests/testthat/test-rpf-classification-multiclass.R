1c5114a7091fed3d1307e8bac1497334ca1c8b42
Lukas Burk <lukas@quantenbrot.de>
2022-03-14 15:21:19 +0100

    Restructure classif tests

---
+++ tests/testthat/test-rpf-classification-multiclass.R
@@ -0,0 +1,41 @@
+# Using iris is suboptimal but it's easy and comes with 3-class y
+# penguins would require package in Suggests and na.omit() preprocessing
+test_that("Multiclass: All numeric", {
+  classif_fit <- rpf(Species ~ ., data = iris)
+
+  expect_s3_class(classif_fit, "rpf")
+  expect_s4_class(classif_fit$fit, "Rcpp_ClassificationRPF")
+})
+
+test_that("Multiclass: All numeric, class predictions", {
+  classif_fit <- rpf(Species ~ ., data = iris)
+
+  classif_pred_class <- predict(classif_fit, iris[, -5], type = "class")
+})
+
+test_that("Multiclass: All numeric, probability predictions", {
+  classif_fit <- rpf(Species ~ ., data = iris)
+
+  classif_pred_prob <- predict(classif_fit, iris[, -5], type = "prob")
+})
+
+test_that("Multiclass: Detection works", {
+  xdat <- data.frame(
+    yint = sample(c(0L, 1L, 2L), 100, replace = TRUE),
+    yfact = factor(sample(c("hi", "mid", "lo"), 100, replace = TRUE)),
+    ychar = sample(c("hi", "mid", "lo"), 100, replace = TRUE),
+    x1 = rnorm(100),
+    x2 = rnorm(100)
+  )
+
+  # y is integer: should _not_ be treated as classif task
+  y_int <- rpf(yint ~ x1 + x2, xdat)
+  expect_failure(expect_s4_class(y_int$fit, "Rcpp_ClassificationRPF"))
+
+  # y 3-level factor
+  y_fact <- rpf(yfact ~ x1 + x2, xdat)
+  expect_s4_class(y_fact$fit, "Rcpp_ClassificationRPF")
+
+  # y 3-level character
+  expect_error(rpf(ychar ~ x1 + x2, xdat))
+})
