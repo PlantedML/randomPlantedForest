4394d832759c0f83cf935faf0a4004bd980d5e68
Marvin Wright <github@wrig.de>
2022-04-12 14:35:04 +0200

    fix profile script

--- profile2.R
+++ profile2.R
@@ -5,7 +5,8 @@ randomPlantedForest:::start_profiler("/tmp/profile.out")
 rpf(Species ~ ., iris, ntrees = 50)
 randomPlantedForest:::stop_profiler()
 
-system("~/go/bin/pprof -svg -nodefraction 0.01 pkg/src/pkg.so /tmp/profile.
-out")
+# Run from terminal to actually write to svg file
+# TODO: Fix to run this from R?
+system("~/go/bin/pprof -svg -nodefraction 0.01 src/randomPlantedForest.so /tmp/profile.out")
 
 rsvg::rsvg_png("profile001.svg", "profile001.png", width = 2000)
