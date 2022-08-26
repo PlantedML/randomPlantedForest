b513d89fd399b465c9c14b4f76eb65d8976ac76f
Lukas Burk <lukas@quantenbrot.de>
2022-08-26 12:10:55 +0200

    Disable another max_interaction test

--- tests/testthat/test-rpf.R
+++ tests/testthat/test-rpf.R
@@ -26,7 +26,7 @@ test_that("recipe interface", {
 
   rec <- recipes::recipe(mpg ~ ., data = mtcars)
   rpf_fit <- rpf(rec, data = mtcars)
-  
+
   expect_s3_class(rpf_fit, "rpf")
   expect_s4_class(rpf_fit$fit, "Rcpp_RandomPlantedForest")
 })
@@ -75,23 +75,24 @@ test_that("Rcpp RNG/R RNG interference", {
 
 # Parameter sets/combinations ---------------------------------------------
 
-test_that("Setting max_interaction = 0 works", {
-  set.seed(100)
-  rpf_fit0 <- rpf(mpg ~ ., data = mtcars[1:20,], max_interaction = 0)
-  pred0 <- predict(rpf_fit0, new_data = mtcars[21:32,])
-  
-  set.seed(100)
-  rpf_fit10 <- rpf(mpg ~ ., data = mtcars[1:20,], max_interaction = 10)
-  pred10 <- predict(rpf_fit10, new_data = mtcars[21:32,])
-  
-  set.seed(100)
-  rpf_fit_default <- rpf(mpg ~ ., data = mtcars[1:20,])
-  pred_default <- predict(rpf_fit_default, new_data = mtcars[21:32,])
-  
-  # Sanity: pred of default (max_interaction = 1) should be different
-  expect_failure(expect_equal(pred_default, pred0))
-  
-  # prediction result should be identical if max_interaction = 0 or
-  # max_interaction = p (= 10 for mtcars, ncol(mtcars) - 1)
-  expect_equal(pred0, pred10)
-})
+# FIXME: Temporarily disable calling max_interaction > 1 due to runtime issue
+# test_that("Setting max_interaction = 0 works", {
+#   set.seed(100)
+#   rpf_fit0 <- rpf(mpg ~ ., data = mtcars[1:20,], max_interaction = 0)
+#   pred0 <- predict(rpf_fit0, new_data = mtcars[21:32,])
+#
+#   set.seed(100)
+#   rpf_fit10 <- rpf(mpg ~ ., data = mtcars[1:20,], max_interaction = 10)
+#   pred10 <- predict(rpf_fit10, new_data = mtcars[21:32,])
+#
+#   set.seed(100)
+#   rpf_fit_default <- rpf(mpg ~ ., data = mtcars[1:20,])
+#   pred_default <- predict(rpf_fit_default, new_data = mtcars[21:32,])
+#
+#   # Sanity: pred of default (max_interaction = 1) should be different
+#   expect_failure(expect_equal(pred_default, pred0))
+#
+#   # prediction result should be identical if max_interaction = 0 or
+#   # max_interaction = p (= 10 for mtcars, ncol(mtcars) - 1)
+#   expect_equal(pred0, pred10)
+# })
