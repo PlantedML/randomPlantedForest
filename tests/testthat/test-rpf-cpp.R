546f3b2bdb809aec2f9892acb59f4554b9a61f32
Lukas Burk <lukas@quantenbrot.de>
2022-08-31 18:21:31 +0200

    Fix too-specific compiler-dependant test

--- tests/testthat/test-rpf-cpp.R
+++ tests/testthat/test-rpf-cpp.R
@@ -3,19 +3,21 @@ test_that("C-level functionality works", {
   train <-  mtcars[1:20, ]
   test <-  mtcars[21:32, ]
 
+  ntrees <- 35
   set.seed(23)
-  rpfit <- rpf(mpg ~., data = train, max_interaction = 3)
+  rpfit <- rpf(mpg ~., data = train, max_interaction = 3, ntrees = ntrees)
   pred <- predict(rpfit, test)
 
   mse <- rpfit$fit$MSE(as.matrix(pred$.pred), as.matrix(test$mpg))
-  checkmate::expect_number(mse)
+  checkmate::expect_number(mse, lower = 5, upper = 30)
   # FIXME: Even with seed, results change between platforms. Yikes.
   # MSE here is ~14 on non-macOS platforms.
-  if (Sys.info()[["sysname"]] == "Darwin") expect_equal(mse, 17.1905463)
+  # Difference probably compiler related, so can't reasonably expect fixed values I think.
+  # if (Sys.info()[["sysname"]] == "Darwin") expect_equal(mse, 17.1905463)
 
   mod <- rpfit$fit$get_model()
   expect_true(inherits(mod, "list"))
-  expect_equal(length(mod), 50)
+  expect_equal(length(mod), ntrees) # length is number of trees
 
   # FIXME: Cosmetic issue but should be addressed at some point
   # This should be assignable instead of printing to stdout
