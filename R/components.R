2f3eee63ec1e213320395afe0616ac582aaf95ab
Lukas Burk <lukas@quantenbrot.de>
2023-01-20 15:47:09 +0100

    Auto-purify and allow intercept retrieval

--- R/components.R
+++ R/components.R
@@ -4,9 +4,9 @@
 #' The sum of all components equals the overall predicted value for an observation.
 #'
 #' * `extract_components` extracts all possible components up to `max_interaction` degrees,
-#'  up to the value set when calling [`rpf()`].
+#'  up to the value set when calling [`rpf()`]. The intercept is always included.
 #' * `extract_component` allows extracting only a single component for a given predictor
-#' or combination of predictors.
+#' or combination of predictors, including the intercept if `predictors` is `NULL`.
 #'
 #' @note
 #' Depending on the number of predictors and `max_interaction`, the number of components will
@@ -14,8 +14,8 @@
 #'
 #' @inheritParams predict.rpf
 #' @param predictors [`character`] Vector of one or more column names of predictor variables
-#'   in `new_data` to extract components for.
-#' @param max_interaction [`integer`] Maximum degree of interactions to consider.
+#'   in `new_data` to extract components for. IF `NULL`, the intercept component is returned.
+#' @param max_interaction [`integer`] or `NULL` Maximum degree of interactions to consider.
 #'   Default will use the `max_interaction` parameter from the [`rpf`] object.
 #'   Must be between `1` (main effects only) and the `max_interaction` of the [`rpf`] object.
 #'
@@ -32,7 +32,7 @@
 #' test <-  mtcars[21:32, 1:4]
 #'
 #' set.seed(23)
-#' rpfit <- rpf(mpg ~ ., data = train, max_interaction = 3, ntrees = 30, purify = TRUE)
+#' rpfit <- rpf(mpg ~ ., data = train, max_interaction = 3, ntrees = 30, purify = FALSE)
 #'
 #' # Extract all components, including main effects and interaction terms up to `max_interaction`
 #' (components <- extract_components(rpfit, test))
@@ -48,9 +48,7 @@
 #'
 #' # The difference is the combined contribution of interaction effects
 #' cbind(rowSums(main_effects), predict(rpfit, test))
-#'
-#' # Component for interaction term of two predictors
-#' extract_component(rpfit, test, predictors = c("cyl", "hp"))
+
 extract_components <- function(object, new_data, max_interaction = NULL) {
   # Get predictor names to keep track of them
   pred_names <- names(object$blueprint$ptypes$predictors)
@@ -70,6 +68,8 @@ extract_components <- function(object, new_data, max_interaction = NULL) {
   })
 
   all_components <- tibble::as_tibble(do.call(cbind, args = all_components))
+  intercept <- extract_component(object, new_data, predictors = NULL)
+  all_components <- cbind(intercept, all_components)
 
   # Remove components with constant 0s
   all_components[, vapply(all_components, function(x) !all(x == 0), FUN.VALUE = logical(1))]
@@ -77,14 +77,34 @@ extract_components <- function(object, new_data, max_interaction = NULL) {
 
 #' @rdname extract_components
 #' @export
+#' @examples
+#' # Regression task, only some predictors
+#' train <-  mtcars[1:20, 1:4]
+#' test <-  mtcars[21:32, 1:4]
+#'
+#' set.seed(23)
+#' rpfit <- rpf(mpg ~ ., data = train, max_interaction = 3, ntrees = 30, purify = FALSE)
+#'
+#' # Component for interaction term of two predictors
+#' extract_component(rpfit, test, predictors = c("cyl", "hp"))
+#'
+#' extract_component(rpfit, test, predictors = NULL)
 extract_component <- function(object, new_data, predictors = NULL) {
 
   # Ensure selected predictors are subset of original predictors
+  # but allow NULL if intercept is requested
   checkmate::assert_subset(
     predictors,
     choices = names(object$blueprint$ptypes$predictors),
-    empty.ok = FALSE
+    empty.ok = TRUE
   )
+
+  # Check if forest is purified, if not we do that now
+  # First, check that purification is reported correctly.
+  checkmate::assert_logical(object$fit$is_purified(), any.missing = FALSE, len = 1)
+  if (!object$fit$is_purified()) {
+    purify(object)
+  }
 
   # Enforces column order, type, column names, etc
   processed <- hardhat::forge(new_data, object$blueprint)
@@ -93,13 +113,17 @@ extract_component <- function(object, new_data, predictors = NULL) {
   new_data <- preprocess_predictors_predict(object, processed$predictors)
 
   # Indices of selected predictors
-  components <- match(predictors, colnames(new_data))
-
-  # subset new_data to contain only selected components
-  new_data <- new_data[, components, drop = FALSE]
+  if (is.null(predictors)) {
+    # Intercept is retrieved by passing -1 to predict_matrix() as a special case
+    components <- -1
+  } else {
+    components <- match(predictors, colnames(new_data))
+    # subset new_data to contain only selected components
+    new_data <- new_data[, components, drop = FALSE]
+  }
 
   ret <- object$fit$predict_matrix(new_data, components)
-  colnames(ret) <- paste0(predictors, collapse = ":")
+  colnames(ret) <- ifelse(is.null(predictors), "intercept", paste0(predictors, collapse = ":"))
 
   tibble::as_tibble(ret)
 }
