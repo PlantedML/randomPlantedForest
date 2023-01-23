3984dae1f4427a2ed5d3cbcedbe38dc58eac3953
Lukas Burk <lukas@quantenbrot.de>
2023-01-23 12:39:12 +0100

    Add is_purified wrapper

--- R/purify.R
+++ R/purify.R
@@ -4,6 +4,7 @@
 #'
 #' Unless [`rpf()`] is called with `purify = TRUE`, the forest has to be purified after fit
 #' to ensure the components extracted by [`extract_components()`] are valid.
+#' [`extract_components()`] will automatically purify a forest if [`is_purified()`] reports `FALSE`.
 #'
 #' @param x And object of class `rpf`.
 #' @param ... (Unused)
@@ -34,4 +35,12 @@ purify.rpf <- function(x, ...) {
   # FIXME: Silence output from C++ by capturing it
   capture.output(x$fit$purify())
   x
+}
+
+#' Check if a forest is purified
+#' @export
+#' @rdname purify
+is_purified <- function(x) {
+  checkmate::assert_class(x, "rpf")
+  x$fit$is_purified()
 }
