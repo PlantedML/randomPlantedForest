dc99eb8ddceb4b0017558cb44ab6b702c37462a8
Lukas Burk <lukas@quantenbrot.de>
2023-01-23 16:40:42 +0100

    Purification tests

--- tests/testthat/test-purify.R
+++ tests/testthat/test-purify.R
@@ -9,7 +9,7 @@ xdat <- data.frame(
 
 xdat$yfact <- factor(rbinom(100, 1, prob = plogis(xdat$x1 + xdat$x2)), labels = c("neg", "pos"))
 
-test_that("Purification works", {
+test_that("purify() and is_purified() work", {
   bin_fit <- rpf(yfact ~ x1 + x2 + x3 + x4 + x5, data = xdat, max_interaction = 3)
 
   expect_false(is_purified(bin_fit))
@@ -19,12 +19,38 @@ test_that("Purification works", {
 
   expect_true(is_purified(bin_fit))
   expect_identical(is_purified(bin_fit), bin_fit$fit$is_purified())
+})
 
-  bin_pred <- predict(bin_fit, new_data = xdat)
-  m <- extract_components(bin_fit, xdat)
-  msum <- rowSums(m)
+test_that("purification does not alter predictions (null effect)", {
+  bin_fit <- rpf(yfact ~ x1 + x2 + x3 + x4 + x5, data = xdat, max_interaction = 3)
+  pred_pre <- predict(bin_fit, new_data = xdat, type = "numeric")
 
-  # Purification seems to affect prediction results, but hard to gauge how much is considered acceptable.
-  expect_lte(sqrt(mean((msum - bin_pred$.pred_pos)^2)), expected = 0.015)
-  # expect_equal(msum, bin_pred$.pred_pos, tolerance = 1e-13)
+  expect_false(is_purified(bin_fit))
+  purify(bin_fit)
+  expect_true(is_purified(bin_fit))
+
+  pred_post <- predict(bin_fit, new_data = xdat, type = "numeric")
+
+  expect_equal(pred_pre, pred_post, tolerance = 1e-15)
+})
+
+test_that("purification does not alter predictions (with effect)", {
+  train <-  mtcars[1:20, 1:4]
+  test <-  mtcars[21:32, 1:4]
+
+  set.seed(23)
+  rpfit <- rpf(mpg ~ ., data = train, max_interaction = 3, ntrees = 30)
+  pred_pre <- predict(rpfit, test)
+
+  # Not purified
+  expect_false(is_purified(rpfit))
+
+  purify(rpfit)
+
+  # Purified
+  expect_true(is_purified(rpfit))
+
+  pred_post <- predict(rpfit, test)
+
+  expect_equal(pred_pre, pred_post, tolerance = 1e-15)
 })
