e44a2fc5bea9b7b61765bd68810f8f4d2642405f
Lukas Burk <lukas@quantenbrot.de>
2023-09-26 14:17:24 +0200

    Whoopsie

--- attic/plot-comp-testing.R
+++
@@ -1,9 +0,0 @@
-# cleaning up components
-library(randomPlantedForest)
-library(data.table)
-
-mtcars$cyl <- factor(mtcars$cyl)
-mtcars$vs <- factor(mtcars$vs)
-
-rpfit <- rpf(mpg ~ cyl + wt + hp + drat + vs, data = mtcars, ntrees = 25, max_interaction = 3)
-rpfit
